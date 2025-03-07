488f5efbaaafccc5a0fe9e9595aa6f30736cfcf5
Anh Phan <stenwolf459@gmail.com>
2025-03-06 16:52:35 -0800

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -74,23 +74,23 @@ function App() {
 
 	return (
 		<main>
-		<div id="game-container">
-			<ol id="players" className="highlight-player">
-				<Player initialName="Player 1"
-					symbol="X"
-					isActive={activePlayer === 'X'}
-					onNameChange={handlePlayerNameChange}
-				/>
-				<Player initialName="Player 2"
-					symbol="O"
-					isActive={activePlayer === 'O'}
-					onNameChange={handlePlayerNameChange}
-				/>
-			</ol>
-			{(winner || hasDraw) && <GameOver winner={winner} onRematch={handleRematch} />}
-			<GameBoard onSelectSquare={handleSelectSquare} board={gameBoard}/>
-		</div>
-		<Log turns={gameTurns} />
+			<div id="game-container">
+				<ol id="players" className="highlight-player">
+					<Player initialName="Player 1"
+						symbol="X"
+						isActive={activePlayer === 'X'}
+						onNameChange={handlePlayerNameChange}
+					/>
+					<Player initialName="Player 2"
+						symbol="O"
+						isActive={activePlayer === 'O'}
+						onNameChange={handlePlayerNameChange}
+					/>
+				</ol>
+				{(winner || hasDraw) && <GameOver winner={winner} onRematch={handleRematch} />}
+				<GameBoard onSelectSquare={handleSelectSquare} board={gameBoard}/>
+			</div>
+			<Log turns={gameTurns} />
 		</main>
 	)
 }
